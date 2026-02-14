d8db41f5d668096767d50998ec34124bb126cc2e
Tuchanski <gui.rocha2004@gmail.com>
2026-02-14 17:22:39 -0300

    chore: initialize project

---
+++ src/main/java/dev/tuchanski/transacoes/TransacoesApplication.java
@@ -0,0 +1,13 @@
+package dev.tuchanski.transacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tuchanski/transacoes/TransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tuchanski.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
