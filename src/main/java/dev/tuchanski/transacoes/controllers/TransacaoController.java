13cbce55750fa5057f3f7f172cd3351c00425f4a
Tuchanski <gui.rocha2004@gmail.com>
2026-02-16 11:56:26 -0300

    feat: add docs

--- src/main/java/dev/tuchanski/transacoes/controllers/TransacaoController.java
+++ src/main/java/dev/tuchanski/transacoes/controllers/TransacaoController.java
@@ -2,6 +2,13 @@ package dev.tuchanski.transacoes.controllers;
 
 import dev.tuchanski.transacoes.dtos.TransacaoRequestDTO;
 import dev.tuchanski.transacoes.services.impl.TransacaoServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
+@Tag(name = "Transações", description = "Endpoints para criação e análise de transações")
 @RestController
 @AllArgsConstructor
 public class TransacaoController {
@@ -17,18 +25,37 @@ public class TransacaoController {
     @Autowired
     private final TransacaoServiceImpl service;
 
+    @Operation(description = "Operação para criar uma transação")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Transação criada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Transação não aceita por não atender 1 ou mais critérios de aceite"),
+            @ApiResponse(responseCode = "400", description = "Corpo da requisição vazio")
+    })
     @PostMapping("/transacao")
-    public ResponseEntity<Void> create(@RequestBody TransacaoRequestDTO dto) {
+    public ResponseEntity<Void> create(@Parameter(description = "Dados da transação a ser criada", required = true) @RequestBody TransacaoRequestDTO dto) {
         this.service.createTransacao(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(description = "Operação para limpar armazenamento em memória")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lista de transações limpa com sucesso")
+    })
     @DeleteMapping("/transacao")
     public ResponseEntity<Void> deleteAll() {
         this.service.clearTransacoes();
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @Operation(description = "Operação para recuperar estatísticas das transações criadas no último minuto")
+    @ApiResponses(
+            @ApiResponse(responseCode = "200", description = "Estatísticas recuperadas com sucesso", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            example = "{ \"count\": 0, \"sum\": 0, \"min\": 0, \"avg\": 0, \"max\": 0 }"
+                    )
+            ))
+    )
     @GetMapping("/estatistica")
     public ResponseEntity<HashMap<String, Number>> getStatsUltimoMin() {
         return ResponseEntity.status(HttpStatus.OK).body(service.getStatsUltimoMinuto());
--- src/main/java/dev/tuchanski/transacoes/dtos/TransacaoRequestDTO.java
+++ src/main/java/dev/tuchanski/transacoes/dtos/TransacaoRequestDTO.java
@@ -1,6 +1,10 @@
 package dev.tuchanski.transacoes.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.OffsetDateTime;
 
-public record TransacaoRequestDTO(Float valor, OffsetDateTime dataHora) {
+public record TransacaoRequestDTO(
+        @Schema(description = "Valor da transação", example = "150.50") Float valor,
+        @Schema(description = "Timestamp da transação", example = "2022-02-16T09:58:30.789-03:00") OffsetDateTime dataHora) {
 }
